d4abf39bc31fdd76912c4be9b55803037e49e61b
shashank-mugiwara <shashank.j@creditsaison-in.com>
2024-02-24 18:02:02 +0530

    main: Running containers in test

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
+	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
+	"github.com/shashank-mugiwara/joyboy/config"
 	"github.com/shashank-mugiwara/joyboy/manager"
 	"github.com/shashank-mugiwara/joyboy/node"
 	"github.com/shashank-mugiwara/joyboy/task"
@@ -13,6 +16,7 @@ import (
 )
 
 func main() {
+	os.Setenv("DOCKER_API_VERSION", "1.44")
 	t := task.Task{
 		ID:     uuid.New(),
 		Name:   "Task-1",
@@ -65,4 +69,53 @@ func main() {
 	}
 
 	fmt.Printf("node: %v\n", n)
+
+	fmt.Printf("Testing create_container")
+	dockerTask, createResult := create_container()
+	if createResult.Error != nil {
+		fmt.Println(createResult.Error.Error())
+		os.Exit(1)
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Printf("Stopping container %s\n", createResult.ContainerId)
+	stop_container(dockerTask)
+}
+
+func create_container() (*task.Docker, *task.DockerResult) {
+	containerConfig := config.Config{
+		Name:  "postgres-container-001-test",
+		Image: "postgres:13",
+		Env: []string{
+			"POSPOSTGRES_USER=joyboy",
+			"POSTGRES_PASSWORD=SamplePassword",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: containerConfig,
+	}
+
+	result := d.Run()
+
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, containerConfig)
+	return &d, &result
+}
+
+func stop_container(d *task.Docker) *task.DockerResult {
+	result := d.Stop(d.ContainerId)
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil
+	}
+
+	fmt.Printf("Container %s has been stopped or removed\n", result.ContainerId)
+	return &result
 }
