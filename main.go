73dfd1a03c6d4e26e75639c36b5a43aba964a16f
shashank-mugiwara <shashank.j@creditsaison-in.com>
2024-03-06 18:56:18 +0530

    main: Refactoring.

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/docker/docker/client"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
-	"github.com/shashank-mugiwara/joyboy/config"
 	"github.com/shashank-mugiwara/joyboy/task"
 	"github.com/shashank-mugiwara/joyboy/worker"
 )
@@ -25,7 +23,7 @@ func main() {
 		ID:    uuid.New(),
 		Name:  "test-container-1",
 		State: task.Scheduled,
-		Image: "strm/helloworld-http",
+		Image: "nginx:mainline-alpine-perl",
 	}
 
 	fmt.Println("starting worker")
@@ -46,44 +44,4 @@ func main() {
 		fmt.Printf("error in stopping the container %v\n", result.Error)
 		panic(result.Error)
 	}
-}
-
-func create_container() (*task.Docker, *task.DockerResult) {
-	containerConfig := config.Config{
-		Name:  "postgres-001",
-		Image: "postgres:12",
-		Env: []string{
-			"POSPOSTGRES_USER=joyboy",
-			"POSTGRES_PASSWORD=SamplePassword",
-		},
-	}
-
-	dc, _ := client.NewClientWithOpts(client.FromEnv)
-	d := task.Docker{
-		Client: dc,
-		Config: containerConfig,
-	}
-
-	result := d.Run()
-
-	if result.Error != nil {
-		fmt.Printf("%v\n", result.Error)
-		return nil, nil
-	}
-
-	fmt.Printf("%v", result)
-
-	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, containerConfig)
-	return &d, &result
-}
-
-func stop_container(d *task.Docker) *task.DockerResult {
-	result := d.Stop(d.ContainerId)
-	if result.Error != nil {
-		fmt.Printf("%v\n", result.Error)
-		return nil
-	}
-
-	fmt.Printf("Container %s has been stopped or removed\n", result.ContainerId)
-	return &result
 }
