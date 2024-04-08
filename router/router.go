795029edc28c92af3aa06b5a8ecf9dce326e3e55
shashank-mugiwara <shashank.j@creditsaison-in.com>
2024-04-08 21:42:52 +0530

    rest-api: Adding router

---
+++ router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func New() *echo.Echo {
+	e := echo.New()
+	e.Logger.SetLevel(log.DEBUG)
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
+	e.Validator = NewValidator()
+	return e
+}
